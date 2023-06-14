629dd8086a65db36d3903974f2bef76614f3d301
Sahil Singh <125428501+Sahil-Singh2002@users.noreply.github.com>
2023-06-14 16:18:01 +0100

    Bernoulli_Numberss.py

--- Bernoulli_Numberss.py
+++ Bernoulli_Numberss.py
@@ -1,37 +1,35 @@
 import math
+import pylab
+
 def bernoulli(m):
     Bm = 0
-    for k in range (m + 1):
-        for v in range (k + 1):
-            Bm += (-1)**v *math.comb(k,v) * (v**m / (k +1)) 
+    for k in range(m + 1):
+        term = 0
+        for v in range(k + 1):
+            term += (-1) ** v * math.comb(k, v) * (v ** m / (k + 1))
+        Bm += term
     return Bm
 
-def pn(n,x):
+def pn(n, x):
     """
     Function that computes the $n$-th McLaurin polynomials for $\tan$ at the point $x$. 
     """
-    # Your code here
-    Total_Sum=0
-    for k in range (1,n + 1):
-        Total_Sum+=((bernoulli(2*k))/math.factorial(2*k))*((-4)**k)*(1-4**k)*x**(2*k-1)
+    Total_Sum = 0
+    for k in range(1, n + 1):
+        term = (bernoulli(2 * k) / math.factorial(2 * k)) * ((-4) ** k) * (1 - 4 ** k) * x ** (2 * k - 1)
+        Total_Sum += term
     return Total_Sum
- 
-import pylab
 
-xv= pylab.linspace(-1.05,1.05,1000)
-xy=pylab.tan(xv)
-p1=[]
-p2=[]
-p3=[]
-for x in xv:
-    p1.append(pn(1,x))
-    p2.append(pn(2,x))
-    p3.append(pn(3,x))
+xv = pylab.linspace(-1.05, 1.05, 1000)
+xy = pylab.tan(xv)
+p1 = [pn(1, x) for x in xv]
+p2 = [pn(2, x) for x in xv]
+p3 = [pn(3, x) for x in xv]
 
-pylab.plot(xv,xy,label="$\\tan(x)$")
-pylab.plot(xv,p1,label="$p1$")
-pylab.plot(xv,p2,label="$p2$")
-pylab.plot(xv,p3,label="$p3$")
+pylab.plot(xv, xy, label="$\\tan(x)$")
+pylab.plot(xv, p1, label="$p1$")
+pylab.plot(xv, p2, label="$p2$")
+pylab.plot(xv, p3, label="$p3$")
 
 pylab.legend(loc='lower right')
 pylab.xlabel('$x$')
